2dbd60f0723c76ca394b338d30aefc51fa553b2a
David Cañadas López <dcanadas@bsc.es>
2025-06-15 12:33:05 +0200

    Mejorar listado de programas a copiar.

--- vm/manage-disk.sh
+++ vm/manage-disk.sh
@@ -3,15 +3,18 @@
 dev="/dev/loop30"
 part="/dev/loop30p1"
 point="vda-mnt"
-code="../src/sw"
+kcode="../src/sw/kernel"
+ucode="../src/sw/userspace"
 
-km=$code/kernel/pnvl.ko
-m0=$code/userspace/master0
-m1=$code/userspace/master1
-c0=$code/userspace/chiplet0
-c1=$code/userspace/chiplet1
-mm=$code/userspace/master-mm
-cm=$code/userspace/chiplet-mm
+PROGS="
+$kcode/pnvl.ko
+$ucode/master0
+$ucode/master1
+$ucode/chiplet0
+$ucode/chiplet1
+$ucode/master-mm
+$ucode/chiplet-mm
+"
 
 # Can run all in sequence with "-iur"
 while getopts "iur" opt; do
@@ -22,13 +25,10 @@ while getopts "iur" opt; do
 			sudo mount -v $part $point
 			;;
 		u) # UPDATE PROGRAMS
-			[ -f $km ] && sudo cp -v $km $point
-			[ -f $m0 ] && sudo cp -v $m0 $point
-			[ -f $c0 ] && sudo cp -v $c0 $point
-			[ -f $m1 ] && sudo cp -v $m1 $point
-			[ -f $c1 ] && sudo cp -v $c1 $point
-			[ -f $mm ] && sudo cp -v $mm $point
-			[ -f $cm ] && sudo cp -v $cm $point
+			for prog in $PROGS
+			do
+				[ -f $prog ] && sudo cp -v $prog $point
+			done
 			;;
 		r) # UNMOUNT DISK IMAGE
 			sudo umount -v $point
